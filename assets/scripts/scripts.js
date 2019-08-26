7b97ef70aa1dbbc22c51c3bfcd761eb8582832a1
curiousbutton <scannermobs@gmail.com>
2019-08-26 11:36:51 +0100

    Add files via upload

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,27 @@
+jQuery(document).foundation();
+/*
+These functions make sure WordPress
+and Foundation play nice together.
+*/
+jQuery(document).ready(function(){// Remove empty P tags created by WP inside of Accordion and Orbit
+jQuery('.accordion p:empty, .orbit p:empty').remove();// Adds Flex Video to YouTube and Vimeo Embeds
+jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function(){if(jQuery(this).innerWidth()/jQuery(this).innerHeight()>1.5){jQuery(this).wrap("<div class='widescreen responsive-embed'/>");}else{jQuery(this).wrap("<div class='responsive-embed'/>");}});});
+jQuery('main table').wrap('<div class="table-scroll" />');
+
+$=jQuery;
+
+//////////////////////////////////////////////////////////
+//      		State for mobile menu button
+
+$(function(){
+	$('#mobile-menu-button').click(function(){
+		$(this).children().toggleClass('hide');
+	});
+});
+
+//////////////////////////////////////////////////////////
+//      		Template to copy and edit
+
+$(function(){
+	
+});
